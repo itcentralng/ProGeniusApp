31951af9a46fd9139328acea0640a3d24f2b802d
saadyakubu <saadyakubu2@yahoo.com>
2023-04-25 11:36:54 +0100

    Added profile image and logout buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,29 @@ import { Typography, Box, colors, Button } from '@mui/material';
 import { LogoutRounded } from '@mui/icons-material';
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
+import mascot from '../components/images/[removal.ai]mascot.png'
 
 function Header() {
     const navigate = useNavigate()
   return (
-    <Box>
-        <Typography variant='h4' sx={{color: 'gold', p: 2, background: '#3C0B79', boxShadow:'0 2px 9px 0 #888888' }} gutterBottom 
+    <Box sx={{color: 'gold', p: 2, background: '#3C0B79', boxShadow:'0 2px 9px 0 #888888' }}>
+        <Typography variant='h4'    
         onClick={()=>navigate('/')}>
             ProGenius
-            <Button sx={{ml:'85rem', color:'#fff'}}><LogoutRounded/></Button>
+            <div style={{marginLeft:'90rem', color:'gold', fontSize: '1.3rem', marginTop: '-3rem'}}>
+                               
+                <Button sx={{color: '#fff'}}>  
+                    <img src={`${mascot}`} width="40px" height="40px" 
+                    style={{borderRadius: '50%', border: '3px solid #fff', boxShadow:'0 2px 9px 0 #888888', 
+                    padding: '.1rem',}}  alt="Profile Image" />                  
+                </Button>
+
+                <Button>
+                    <LogoutRounded sx={{color: '#fff'}}/>
+                </Button>
+            </div>
         </Typography>
+        
     </Box>
   )
 }
