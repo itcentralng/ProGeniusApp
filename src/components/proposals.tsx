7caad917eefb675539558c38309b4f66aa11e4d1
saadyakubu <saadyakubu2@yahoo.com>
2023-04-27 07:09:25 +0100

    Add new proposal link

--- src/components/proposals.tsx
+++ src/components/proposals.tsx
@@ -2,7 +2,7 @@ import { Grid, Typography, Box, Button } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import Header from './header'
 import ProposalCard from './proposalcard'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import Mascot from './mascot'
 import { AddBusinessTwoTone, AddBoxOutlined, AddCardRounded } from '@mui/icons-material';
 import Masonry from '@mui/lab/Masonry';
@@ -13,6 +13,8 @@ function Proposals() {
     const location = useLocation();
     console.log(`Proposals Location: ${location.state}`)
     //const proposals:[] = location.state;
+
+    const navigate = useNavigate();
 
     const [width, setWidth] = useState(window.innerWidth);
 
@@ -74,7 +76,7 @@ function Proposals() {
                     <Box sx={{background:'#3C0B79', boxShadow: '0 2px 9px 0 #888888', color: '#fff', border:'1px solid #fff'}}>
                         <Typography variant='h4' sx={{p:2, textAlign: 'center', color: 'gold'}}>
                             My Proposals
-                            <Button sx={{color:'#fff'}}><AddCardRounded sx={{fontSize:'2rem'}}/></Button>
+                            <Button sx={{color:'#fff'}}   onClick={()=>navigate('/document/new')}><AddCardRounded sx={{fontSize:'2rem'}}/></Button>
                         </Typography>                        
                         <Mascot/>
                         <Typography variant='h5' sx={{p:2, textAlign: 'center'}}>Total: {proposals.length}</Typography>
@@ -95,21 +97,7 @@ function Proposals() {
                     // </Grid>  
                                            
                 ))}
-
-                {/*proposals.map((proposal:any)=>(
-                     
-                    <Grid key={proposal?.id} item xs={12} sm={3} md={3} lg={3}>
-                        <>
-                        <ProposalCard id={proposal?.id} title={proposal?.offering} description={proposal?.description} client_email={proposal?.client.email} 
-                        client_address={proposal?.client.address} client_updated_at={proposal?.client.updated_at} client_logo={proposal?.client.logo} 
-                        client_name={proposal?.client.name} client_rep={proposal?.client.rep} client_role={proposal?.client.role} 
-                        components_content={proposal?.components[4].content}
-                        /> 
-                        {console.log(`Id: ${proposal.id} ${proposal.client.name}`)}
-                        </>
-                    </Grid>  
-                                           
-                ))*/}                
+                             
             </Masonry>
             </Grid> 
             
