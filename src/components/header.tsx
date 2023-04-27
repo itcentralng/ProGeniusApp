7020a1f512d72d5dac634a2ac0e514747922e5be
saadyakubu <saadyakubu2@yahoo.com>
2023-04-27 22:23:09 +0100

    Commit Auth in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,59 @@
 import { Typography, Box, colors, Button } from '@mui/material';
-import { LogoutRounded } from '@mui/icons-material';
-import React from 'react'
-import {useNavigate} from 'react-router-dom'
-import mascot from '../components/images/[removal.ai]mascot.png'
+import { LogoutRounded, LoginRounded } from '@mui/icons-material';
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import mascot from '../components/images/[removal.ai]mascot.png';
+import logo from '../components/images/logo.png';
 
 function Header() {
-    const navigate = useNavigate()
-  return (
-    <Box sx={{color: 'gold', p: 2, background: '#3C0B79', boxShadow:'0 2px 9px 0 #888888' }}>
-        <Typography variant='h4'    
-        onClick={()=>navigate('/')}>
-            ProGenius
-            <div style={{marginLeft:'90rem', color:'gold', fontSize: '1.3rem', marginTop: '-3rem'}}>
-                               
-                <Button sx={{color: '#fff'}}>  
-                    <img src={`${mascot}`} width="40px" height="40px" 
-                    style={{borderRadius: '50%', border: '3px solid #fff', boxShadow:'0 2px 9px 0 #888888', 
-                    padding: '.1rem',}}  alt="Profile Image" />                  
-                </Button>
+    const navigate = useNavigate();
+    const user = localStorage.getItem('email');
+    
+    const logout = ()=>{
+        localStorage.removeItem('token');
+        localStorage.removeItem('email');
+        navigate('/login')
+    }
 
-                <Button>
-                    <LogoutRounded sx={{color: '#fff'}}/>
-                </Button>
+    useEffect(()=>{
+        !user && navigate('/login');
+    },[])
+
+    return (
+        <Box sx={{ color: 'gold', p: 2, px: 20, background: '#000040'/*background:'#3C0B79', boxShadow:'0 2px 9px 0 #888888'*/ }}>
+           
+            <div style={{ color: 'gold', fontSize: '1.3rem', }}>
+                <img src={logo} width='25rem' style={{ marginLeft: '1rem' }} />
+                <Button sx={{ color: 'gold', fontSize: '1.2rem' }} onClick={() => navigate('/')}>ProGenius</Button>
+
+                {user && (
+                    <>
+                        <Button sx={{ ml: 120, color: '#fff' }}>
+                            <img src={`${mascot}`} width="40px" height="40px"
+                                style={{
+                                    borderRadius: '50%', border: '3px solid #fff', boxShadow: '0 2px 9px 0 #888888',
+                                    padding: '.1rem',
+                                }} alt="Profile Image" />
+                        </Button>
+                        <Button sx={{ px: 2, color: '#fff', border: '1.5px solid white', boxShadow: '0 2px 9px 0 #888888' }} onClick={() => logout()}>
+                            Logout <LogoutRounded sx={{ color: '#fff' }} />
+                        </Button>
+                    </>
+                )}
+
+                {/* {!user && 
+                    (
+                    <Button sx={{ ml: 120, px: 2, color: '#fff', border: '1.5px solid white', boxShadow: '0 2px 9px 0 #888888' }} onClick={() => navigate('/login')}>
+                        Login <LoginRounded sx={{ color: '#fff' }} />
+                    </Button>
+                    ) 
+                } */}
+                
             </div>
-        </Typography>
-        
-    </Box>
-  )
+
+
+        </Box>
+    )
 }
 
 export default Header;
